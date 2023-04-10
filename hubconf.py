c2d7b9477a918caa339a53f77933c63cb05473d4
Alan Savio Paul <alan.s.paul09@gmail.com>
2023-04-10 20:35:17 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -8,7 +8,7 @@ def calib(pretrained=True):
     print(os.path.exists('./weights/checkpoint_best.tar'))
     print(os.listdir('.'))
     if not os.path.exists('./weights/checkpoint_best.tar'):
-        os.system('mkdir weights')
+        os.system('mkdir -p weights')
         os.system('wget https://github.com/AlanSavio25/DeepSingleImageCalibration/releases/download/v1/checkpoint_best.tar -P weights') 
     experiment = 'weights'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
