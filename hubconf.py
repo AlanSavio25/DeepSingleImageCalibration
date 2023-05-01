d0837997ae7b1684a48d4c58bd468bc2c1e8d8ef
AlanSavio25 <alan.s.paul09@gmail.com>
2023-05-01 17:52:10 +0200

    replace wget with torch hub load

--- hubconf.py
+++ hubconf.py
@@ -4,11 +4,11 @@ from calib.calib.utils.experiments import load_experiment
 import os
 
 
-def calib(pretrained=True):
+def calib():
     if not os.path.exists('./weights/checkpoint_best.tar'):
         os.system('mkdir -p weights')
-        os.system('wget https://github.com/AlanSavio25/DeepSingleImageCalibration/releases/download/v1/checkpoint_best.tar -P weights') 
+        torch.hub.download_url_to_file('https://github.com/AlanSavio25/DeepSingleImageCalibration/releases/download/v1/checkpoint_best.tar', 
+                'weights/checkpoint_best.tar', hash_prefix='a84cb9606931529bab33524b15cbfd7370b4d7593e2849b3f1dac0b9b3dd2583')
     experiment = 'weights'
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = load_experiment(experiment, {'name': 'densenet'}).eval()
+    model = load_experiment(experiment, {'name': 'densenet'})
     return model
