2af83182698f02136d59841cf38b627b672c00a2
AlanSavio25 <alan.s.paul09@gmail.com>
2023-05-08 08:23:19 +0200

    linter

--- calib/calib/run.py
+++ calib/calib/run.py
@@ -15,12 +15,12 @@ import os
 
 def main():
     # Parse input parameters
-    parser = argparse.ArgumentParser(prog='Deep Single Image calibration', description='Inference code to show the predicted camera parameters')
+    parser = argparse.ArgumentParser(prog='Deep Single Image calibration',
+                                     description='Inference code to show the predicted camera parameters')
     parser.add_argument('--img_dir', default='images/')
     parser.add_argument('--weights_dir', default='weights/')
-    parser.add_argument('--viz_mode', default='grid', help='Grid stores images in grids. Single saves single images.')
     args = parser.parse_args()
-    
+
     os.system('mkdir -p ./results/')
     data_dir = args.img_dir
     results_dir = './results/'
@@ -28,10 +28,12 @@ def main():
     experiment = args.weights_dir
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    roll_centers, rho_centers, fov_centers, k1_hat_centers = get_bin_centers(device)
+    roll_centers, rho_centers, fov_centers, k1_hat_centers = get_bin_centers(
+        device)
     conf = 'calib/calib/configs/config_test.yaml'
     logger.info(f'Starting test {experiment}')
-    conf = OmegaConf.merge(OmegaConf.load(conf), {'data': {'data_dir': data_dir}})
+    conf = OmegaConf.merge(OmegaConf.load(
+        conf), {'data': {'data_dir': data_dir}})
     dataset = get_dataset(conf.data.name)(conf.data)
     test_loader = dataset.get_data_loader('test')
     model = load_experiment(experiment, conf.model).eval()
@@ -44,14 +46,16 @@ def main():
         entropy = compute_categorical_entropy(output)
         pred['roll_entropy'] = entropy
         pred_class = output.argmax(1)
-        pred_deg = torch.tensor(roll_centers[pred_class], dtype=torch.float64, device=device)
+        pred_deg = torch.tensor(
+            roll_centers[pred_class], dtype=torch.float64, device=device)
         pred['roll'] = pred_deg
 
         output = pred['rho']
         entropy = compute_categorical_entropy(output)
         pred['rho_entropy'] = entropy
         pred_class = output.argmax(1)
-        pred_norm_rho = torch.tensor(rho_centers[pred_class], dtype=torch.float64, device=device)
+        pred_norm_rho = torch.tensor(
+            rho_centers[pred_class], dtype=torch.float64, device=device)
         pred_ratio_rho = pred_norm_rho * 0.35
         pred['rho'] = pred_ratio_rho
 
@@ -59,7 +63,8 @@ def main():
         entropy = compute_categorical_entropy(output)
         pred['vfov_entropy'] = entropy
         pred_class = output.argmax(1)
-        pred_fov_deg = torch.tensor(fov_centers[pred_class], dtype=torch.float64, device=device)
+        pred_fov_deg = torch.tensor(
+            fov_centers[pred_class], dtype=torch.float64, device=device)
         pred['fov'] = pred_fov_deg
         del pred['vfov']
 
@@ -67,19 +72,24 @@ def main():
         entropy = compute_categorical_entropy(output)
         pred['k1_hat_entropy'] = entropy
         pred_class = output.argmax(1)
-        pred_k1_hat = torch.tensor(k1_hat_centers[pred_class], dtype=torch.float64, device=device)
+        pred_k1_hat = torch.tensor(
+            k1_hat_centers[pred_class], dtype=torch.float64, device=device)
         pred['k1_hat'] = pred_k1_hat
         result = {'path': data['path'],
-                  **{'pred_'+str(pred_key):pred[pred_key].unsqueeze(0).cpu().item() # TODO: Clean this
-                      if isinstance(pred[pred_key],torch.Tensor)
+                  **{'pred_'+str(pred_key): pred[pred_key].unsqueeze(0).cpu().item()
+                      if isinstance(pred[pred_key], torch.Tensor)
                       else pred[pred_key]
                       for pred_key in pred}
-                        }
-        results.append({ **result, 'image': data['image'].numpy().squeeze(0).transpose((1, 2, 0))})
-        pd.DataFrame(result).T.to_csv(Path(results_dir,str(Path(data['path'][0]).stem)+'.csv'), header=False, index=True)
+                  }
+        results.append(
+            {**result, 'image': data['image'].numpy().squeeze(0).transpose((1, 2, 0))})
+        pd.DataFrame(result).T.to_csv(Path(results_dir, str(
+            Path(data['path'][0]).stem)+'.csv'), header=False, index=True)
     for i in range(len(results)):
-        p = plot_row([results[i]], pred_annotate=['roll','rho', 'fov', 'k1_hat'], titles=[])
+        p = plot_row([results[i]], pred_annotate=[
+                     'roll', 'rho', 'fov', 'k1_hat'], titles=[])
         p.savefig(results_dir + results[i]['path'][0].split('/')[-1])
+
 
 if __name__ == '__main__':
     main()
--- quick.py
+++ quick.py
@@ -1,4 +1,5 @@
 import torch
 
-_, results = torch.hub.load('AlanSavio25/DeepSingleImageCalibration', 'calib', image_path='images/video1-00150.jpg', force_reload=True)
+_, results = torch.hub.load('AlanSavio25/DeepSingleImageCalibration',
+                            'calib', image_path='images/video1-00150.jpg', force_reload=True)
 print(results)
