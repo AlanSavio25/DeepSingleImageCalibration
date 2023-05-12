ea961c630638599ac9e79694e112db4fba531014
Alan Savio Paul <alan.s.paul09@gmail.com>
2023-05-12 12:10:37 +0200

    Update settings.py

--- calib/settings.py
+++ calib/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 root = Path(__file__).parent #.parent  # top-level directory
 #DATA_PATH = root / 'calib' / 'minidata/'
-DATA_PATH = ''
+DATA_PATH = 'path/to/folder/containing/{data_dir}' # data_dir should be set in calib/calib/configs/config_train.py. data_dir folder should contain *.json, *.jpg.
 DATASETS_PATH = root / 'calib' / 'datasets/'  # datasets and pretrained weights
-TRAINING_PATH = ''  # training checkpoints
+TRAINING_PATH = ''  # training checkpoints. This is where the training logs and checkpoints are stored.
 EVAL_PATH = ''  # evaluation results
